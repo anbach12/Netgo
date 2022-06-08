43be3921efb7c431d4245ffc3727290203464701
anbach12 <107155535+anbach12@users.noreply.github.com>
2022-06-08 18:48:20 -0500

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,107 @@
+<?php
+
+date_default_timezone_set ('America/Sao_Paulo'); // define timestamp padrão
+
+// Incluindo arquivos nescessários
+include __DIR__.'/Telegram.php';
+
+if (!file_exists('dadosBot.ini')){
+
+	echo "Faça a instalação do bot antes!";
+	exit;
+
+}
+
+$textoMsg=json_decode (file_get_contents('textos.json'));
+$iniParse=parse_ini_file('dadosBot.ini');
+
+$ip=$iniParse ['ip'];
+$token=$iniParse ['token'];
+$limite=$iniParse ['limite'];
+
+define ('TOKEN', $token); // token do bot criado no @botfather
+
+// Instancia das classes
+$tlg=new Telegram (TOKEN);
+$redis=new Redis ();
+$redis->connect ('localhost', 6379); //redis usando porta padrão
+
+// BLOCO USADO EM LONG POLLING
+
+while (true){
+
+$updates=$tlg->getUpdates();
+
+for ($i=0; $i < $tlg->UpdateCount(); $i++){
+
+$tlg->serveUpdate($i);
+
+switch ($tlg->Text ()){
+
+	case '/start':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => $textoMsg->start,
+		'parse_mode' => 'html',
+		'reply_markup' => $tlg->buildInlineKeyBoard ([
+			[$tlg->buildInlineKeyboardButton ('PRUEBA NETGO POR 24 HORAS', null, '/sshgratis')]
+		])
+	]);
+
+	break;
+	case '/sobre':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => 'Bot Mod @batmonn por @carnalminerdso'
+	]);
+
+	break;
+	case '/total':
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => 'Foram criadas <b>'.$redis->dbSize ().'</b> contas nas ultimas 24h',
+		'parse_mode' => 'html'
+	]);
+
+	break;
+	case '/sshgratis':
+
+	$tlg->answerCallbackQuery ([
+	'callback_query_id' => $tlg->Callback_ID()
+	]);
+
+	if ($redis->dbSize () == $limite){
+
+		$textoSSH=$textoMsg->sshgratis->limite;
+
+	} elseif ($redis->exists ($tlg->UserID ())){
+
+		$textoSSH=$textoMsg->sshgratis->nao_criado;
+
+	} else {
+
+		$usuario=substr (str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 6);
+		$senha=mt_rand(11111, 999999);
+
+		exec ('./gerarusuario.sh '.$usuario.' '.$senha.' 1 1');
+
+		$textoSSH="Cuenta creada para NETGO VPN ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Contraseña:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Valido:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n🤙 Cortesia de @anbach12";
+
+		$redis->setex ($tlg->UserID (), 43200, 'true'); //define registro para ser guardado por 12h
+
+	}
+
+	$tlg->sendMessage ([
+		'chat_id' => $tlg->ChatID (),
+		'text' => $textoSSH,
+		'parse_mode' => 'html'
+	]);
+
+	break;
+
+}
+
+}}
